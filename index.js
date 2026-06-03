7791a641d7cd35bda743226682b2f258b82e39dd
ncd282005 <ncd635473@gmail.com>
2026-06-03 15:43:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker CI/CD Server 🚀');
+  res.send('Hello from Docker CI/CD Server ');
 });
 
 app.get('/health', (req, res) => {
